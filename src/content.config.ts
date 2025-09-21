58abc85b1c8dbcdec17cde2b945bc48daf5ea63b
punieth <punithrathnakar@Puniths-MacBook-Pro-2.local>
2025-09-21 10:39:00 -0400

    adding the first post

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.md', base: "./src/data/blog" }),
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/data/blog" }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
