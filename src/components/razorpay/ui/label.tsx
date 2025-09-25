f7e196249f26f98d3be6e02c19bd7c03fcabbb7a
punieth <punithrathnakar@Puniths-MacBook-Pro-2.local>
2025-09-25 19:42:43 -0400

    fix build issues

--- src/components/razorpay/ui/label.tsx
+++ src/components/razorpay/ui/label.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import * as React from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
 
 import { cn } from "./utils";
 
-function Label({
-  className,
-  ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
-  return (
-    <LabelPrimitive.Root
+type LabelProps = React.LabelHTMLAttributes<HTMLLabelElement>;
+
+const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, ...props }, ref) => (
+    <label
+      ref={ref}
       data-slot="label"
       className={cn(
         "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
@@ -18,7 +17,9 @@ function Label({
       )}
       {...props}
     />
-  );
-}
+  ),
+);
+
+Label.displayName = "Label";
 
 export { Label };
